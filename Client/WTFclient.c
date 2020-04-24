88513f258ee35281e5bc485627a46b90c6350c0a
walrm <bpl52@ilab1.cs.rutgers.edu>
2020-04-24 19:14:25 -0400

    not sure

--- Client/WTFclient.c
+++ Client/WTFclient.c
@@ -73,12 +73,10 @@ int checkConnection()
     free(fileInfo);
     free(ip);
     free(host);
-    //write(sockfd, "test123", 7);
-    char s[16];
-    read(sockfd, s, 16);
-    printf("%s\n", s);
-    char str[100];
-    int i;
+    // write(sockfd, "test123", 7);
+    
+    // char str[100];
+    // int i;
     return sockfd;
 }
 
@@ -96,8 +94,20 @@ void create(char *projectName)
         return;
     }
     int writeLen = 7 + strlen(projectName);
-    write(socketFD, ("create %s", projectName), 7 + strlen(projectName));
+    //  write(socketFD, ("create %s", projectName), 7 + strlen(projectName));
+    char* combined=malloc(writeLen);
+    bzero(combined,writeLen);
+    strcat(combined,"create ");
+    strcat(combined,projectName);
+    printf("%s\n",combined);
+    write(socketFD, combined,writeLen);
     char created[1];
+
+    char s[17];
+    read(socketFD, s, 16);
+    s[17] = '\0';
+    printf("%s\n", s);
+
     read(socketFD, created, 1);
     int c = atoi(created);
     if (!c)
@@ -108,7 +118,7 @@ void create(char *projectName)
     else
     {
         int sys = system(("mkdir \"%s\"", projectName)); //sys is 0 if the directory is created successfully. Might use later to print error, but probably not
-        printf("Project created on the server");
+        printf("Project created on the server\n");
     }
     char bytesofIntC[1];
     read(socketFD, bytesofIntC, 1);
--- Server/serverFunctions.c
+++ Server/serverFunctions.c
@@ -6,9 +6,10 @@
 
 #include "serverFunctions.h"
 
-//TODO: writing server side create function; Need to free 2 memory allocations
+//TODO: writing server side create function; Need to free 3 memory allocations
 void create(char* projectName, int socket){
     //opens (repository) directory from root path and looks if project already exists
+    char* manifestPath;
     DIR *cwd = opendir("./");
     struct dirent *currentINode = NULL;
     do{
@@ -17,32 +18,42 @@ void create(char* projectName, int socket){
             if (strcmp(currentINode->d_name, ".") == 0 || strcmp(currentINode->d_name, "..") == 0)
                     continue;
             printf("%s\n",currentINode->d_name);
+            //Let client know there was an error, project already exists with name
             if(strcmp(currentINode->d_name,projectName)==0){
-                write(socket,"0",1);
-                printf("ERROR SENT");
+                write(socket,"0",1); 
+                printf("ERROR SENT\n");
                 return;
             }
         }
     }while(currentINode!=NULL); //project doesn't exist
+    write(socket,"1",1);
     mkdir(projectName,0700); //creates project with parameter projectName
-    char* manifestPath = malloc(strlen(projectName)+13);
+    manifestPath = malloc(strlen(projectName)+13);
     manifestPath[0] = '.';
     manifestPath[1] = '/';
     strcat(manifestPath,projectName);
-    // char* projectPath = malloc(strlen(projectName)+3);
-    // strcpy(projectPath, manifestPath);
-    // printf("PROJECT PATH: %s\n", projectPath);
-    char manifest[10] = "/.Manifest";
-    strcat(manifestPath,manifest);
+    char m[10] = "/.Manifest";
+    strcat(manifestPath, m);
     printf("PROJECT PATH to Manifest: %s\n", manifestPath);
+
     int manifestFD = open(manifestPath, O_CREAT | O_RDWR, 00777); //Create .Manifest file
     write(manifestFD,"0\n",1);  //Writing version number 0 on the first line
 
-    //Send information over to client socket?
+    struct stat manStats;
     manifestFD = open(manifestPath, O_RDONLY);
-    
-    
-    //writeProjPath(projectPath, manifestFD);
+    if(stat(manifestPath,&manStats)<0){
+        pError("ERROR reading manifest stats");
+    }
+
+    int size = manStats.st_size; //size of manifest file
+    int bytesRead = 0, bytesToRead = 0;
+    char manBuffer[256];
+    while(size > bytesRead){
+        bytesToRead = (size-bytesRead<256)? size-bytesRead : 255;
+        bzero(manBuffer,256);
+        bytesRead += read(manifestFD,manBuffer,bytesToRead);
+        write(socket,manBuffer,bytesToRead);
+    }
 }
 
 // void writeProjPath(char* projectPath, int manifestFD){
