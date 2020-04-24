6cdb3a2ff6aa0682a417377a4c336fd325835e37
walrm <bpl52@ilab1.cs.rutgers.edu>
2020-04-23 21:18:51 -0400

    started create method for server side

--- Server/WTFserver.c
+++ Server/WTFserver.c
@@ -3,9 +3,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <strings.h>
-
+#include <string.h>
+#include <dirent.h>
 #include<pthread.h>
+
+struct thread{
+    int threadNum;
+    int threadsocket;
+};
 
 //Method to print error and return -1 as error
 void pError(char* err){
@@ -15,7 +20,9 @@ void pError(char* err){
 
 //Handles communication between the server and client socket
 void* clientServerInteract(void* socket_arg){
-    int socket = *(int *) socket_arg;
+    struct thread test = *(struct thread*)socket_arg;
+    int socket = test.threadsocket;
+    //int socket = *(int *) socket_arg;
     int status;
     char buffer[256];
     
@@ -23,10 +30,32 @@ void* clientServerInteract(void* socket_arg){
     status = read(socket, buffer, 255);
     if(status < 0)
         pError("ERROR reading from socket");
-    printf("Message From Client: %s\n",buffer);
+    printf("Message From Client %d: %s\n",test.threadNum,buffer);
     status = write(socket, "Message Received", 16);
     if(status < 0)
         pError("ERROR writing to socket");
+
+
+    //TODO: writing server side create function
+    char* projectName;
+    memcpy(projectName, &buffer[7], strlen(buffer)-6);
+    projectName[strlen(projectName)]='\0'; //Grabbing project name from the client side
+    printf("project name: %s\n", projectName);
+
+    //opens (repository) directory from root path
+    DIR *cwd = opendir("./");          
+    struct dirent *currentINode = NULL;
+    do{
+        currentINode = readdir(cwd);
+        if(currentINode!=NULL && currentINode->d_type == DT_DIR){
+            if (strcmp(currentINode->d_name, ".") == 0 || strcmp(currentINode->d_name, "..") == 0)
+                    continue;
+            printf("%s\n",currentINode->d_name);
+            if(strcmp(currentINode->d_name,projectName)==0)
+                pError("ERROR. Project already exists in repository");
+        }
+    }while(currentINode!=NULL);
+    mkdir(projectName,0700); //Project doesn't already exist, create project folder
 }
 
 int main(int argc, char* argv[]){
@@ -40,13 +69,14 @@ int main(int argc, char* argv[]){
     if(socketfd < 0)
         pError("ERROR opening socket");
     
-    //Create sock
+    //Create socket struct and fill information
     bzero((char *) &serv_addr, sizeof(serv_addr));
     port = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
+    //Print hostname in order to connect
     char hostname[1024];
     gethostname(hostname,1024);
     printf("%s\n",hostname);
@@ -56,20 +86,21 @@ int main(int argc, char* argv[]){
         pError("ERROR on bind");
     
     listen(socketfd,5); //socket accepts connections and has maximum backlog of 5
-    
-    char hostname[1024];
-    gethostname(hostname,1024);
-    printf("%s\n",hostname);
-
     clilen = sizeof(cli_addr);
     pthread_t threadID;
+    int i = 1;
+
     //Client trying to connect and forks for each new client connecting
     while(1){
         newsocketfd = accept(socketfd, (struct sockaddr *) &cli_addr, &clilen);
         if(newsocketfd<0)
             pError("ERROR on accept");
+            
+        struct thread t;
+        t.threadNum = i++;
+        t.threadsocket = newsocketfd;
         
-        if(pthread_create(&threadID,NULL,clientServerInteract,(void*)&newsocketfd) < 0)
+        if(pthread_create(&threadID,NULL,clientServerInteract,&t) < 0)
             pError("ERROR on creating thread");
             
         // if(pid == 0){
